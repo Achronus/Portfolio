46c47037b0ddc9ab9b7fb853c745567888ce1815
Ryan Partridge <rpartridge101@yahoo.co.uk>
2021-08-28 20:37:30 +0100

    Removed notes-app

--- advanced/notes_app/website/routes/errors.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from flask_login import current_user
-
-err = Blueprint('err', __name__)
-
-@err.errorhandler(404)
-def page_not_found(e):
-  """Handles pages that cannot be found (404 error)."""
-  return render_template('404.html', user=current_user), 404
